15478542a1bf5dd26fbb9583847be18b44a5fbcf
kenes <kenesrd@mail.ru>
2019-10-04 15:13:28 +0600

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,63 +1,128 @@
 'use strict';
 
-const checkbox = document.querySelectorAll('.filter-check_checkbox'),
-      btnCart = document.getElementById('cart'),
-      modalCart = document.querySelector('.cart'),
-      cartClose = document.querySelector('.cart-close'),
-      countGoods = document.querySelector('.counter');
+function toggleCheckbox() {
+    const checkbox = document.querySelectorAll('.filter-check_checkbox');
+    checkbox.forEach(function (el) {
+        el.addEventListener('change', function () {
+            if (this.checked) {
+                this.nextElementSibling.classList.add('checked');
+            } else {
+                this.nextElementSibling.classList.remove('checked');
+            }
+        });
+    });
+}
 
 
-checkbox.forEach(function(el){ 
-    el.addEventListener('change', function (){
-        if(this.checked){
-            this.nextElementSibling.classList.add('checked');
-        } else {
-            this.nextElementSibling.classList.remove('checked');
-        }
-    });    
-});
 
-// Basket
- btnCart.addEventListener('click', () => {
-     modalCart.style.display = 'flex';
-     document.body.style.overflow = 'hidden';
-});
 
- cartClose.addEventListener('click', () => {
-     modalCart.style.display = 'none';
-     document.body.style.overflow = '';
+function toggleCart() {
+    const btnCart = document.getElementById('cart'),
+        modalCart = document.querySelector('.cart'),
+        cartClose = document.querySelector('.cart-close');
+        
+
+    // Basket
+    btnCart.addEventListener('click', () => {
+        modalCart.style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+    });
+
+    cartClose.addEventListener('click', () => {
+        modalCart.style.display = 'none';
+        document.body.style.overflow = '';
     });
     // end basket
+}
 
+
+function addToCart() {
     // add goods card to basket
-    const cards =document.querySelectorAll('.goods .card'),
-      cartWrapper = document.querySelector('.cart-wrapper'),
-      cartEmpty = document.getElementById('cart-empty');
+    const cards = document.querySelectorAll('.goods .card'),
+        cartWrapper = document.querySelector('.cart-wrapper'),
+        cartEmpty = document.getElementById('cart-empty'),
+        countGoods = document.querySelector('.counter');
 
     cards.forEach((card) => {
         const btn = card.querySelector('button');
-         btn.addEventListener('click', () => {
+        btn.addEventListener('click', () => {
             const cardClone = card.cloneNode(true);
-                cartWrapper.appendChild(cardClone);
-                cartEmpty.remove();
+
+            cartWrapper.appendChild(cardClone);
+
             showData();
-         });
+            const removeBtn = cardClone.querySelector('.btn');
+            removeBtn.textContent = 'Удалить из корзины';
+
+            removeBtn.addEventListener('click', () => {
+                cardClone.remove();
+                showData();
+            });
+        });
     });
 
-    function showData(){
+    function showData() {
         const cardsCart = cartWrapper.querySelectorAll('.card'),
             cardsPrice = cartWrapper.querySelectorAll('.card-price'),
             cardTotal = document.querySelector('.cart-total span');
         let sum = 0;
 
         countGoods.textContent = cardsCart.length;
-        console.log(countGoods);
-        
 
-        cardsPrice.forEach(function(cardPrice){
-            let price = parseFloat(cardPrice.textContent);            
+
+        cardsPrice.forEach(function (cardPrice) {
+            let price = parseFloat(cardPrice.textContent);
             sum += price;
         });
         cardTotal.textContent = sum;
+
+        if (cardsCart.length !== 0) {
+            cartEmpty.remove();
+        } else {
+            cartWrapper.appendChild(cartEmpty);
+        }
     }
     // end goods card to basket
+}
+
+
+// filter action
+function actionPage() {
+    const cards = document.querySelectorAll('.goods .card'),
+        discountCheckbox = document.getElementById('discount-checkbox'),
+        min = document.getElementById('min'),
+        max = document.getElementById('max');
+
+    discountCheckbox.addEventListener('click', () => {
+        cards.forEach((card) => {
+            if(discountCheckbox.checked){
+                if(!card.querySelector('.card-sale')){
+                    card.parentNode.style.display = 'none';
+                }
+            } else {
+                card.parentNode.style.display = '';
+            }
+        });
+    });
+
+    function filterPrice() {
+        
+    }
+
+    min.addEventListener('change', filterPrice);
+    max.addEventListener('change', filterPrice);
+
+
+}
+
+// end filter action
+
+
+toggleCheckbox();
+toggleCart();
+addToCart();
+actionPage();
+
+
+
+
